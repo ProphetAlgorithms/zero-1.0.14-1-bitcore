880fbbfe9c3c1c3cb81d260661e97ec3fb012b86
ProphetAlgorithms <38349760+ProphetAlgorithms@users.noreply.github.com>
2018-06-22 14:56:38 +0200

    Add `listaddresses` rpc command
    commit: https://github.com/ZencashOfficial/zen/pull/45/commits/02dfc7b623ff7dae76262f1a54f9c6988a35ad95

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -229,6 +229,7 @@ extern UniValue createmultisig(const UniValue& params, bool fHelp);
 extern UniValue listreceivedbyaddress(const UniValue& params, bool fHelp);
 extern UniValue listreceivedbyaccount(const UniValue& params, bool fHelp);
 extern UniValue listtransactions(const UniValue& params, bool fHelp);
+extern UniValue listaddresses(const UniValue& params, bool fHelp); // in rpcwallet.cpp
 extern UniValue listaddressgroupings(const UniValue& params, bool fHelp);
 extern UniValue listaccounts(const UniValue& params, bool fHelp);
 extern UniValue listsinceblock(const UniValue& params, bool fHelp);
